9df2670063fb511f752484dec58e06d881a50ac1
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-04-26 15:30:42 +0500

    Adding a set page size option

--- frontent/web-app/app/_actions/auctionActions.ts
+++ frontent/web-app/app/_actions/auctionActions.ts
@@ -2,8 +2,13 @@
 
 import { Auction, PagedResult } from '@/types';
 
-export const getData = async (pageNumber: number = 1): Promise<PagedResult<Auction>> => {
-  const result = await fetch(`http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`);
+export const getData = async (
+  pageNumber: number,
+  pageSize: number,
+): Promise<PagedResult<Auction>> => {
+  const result = await fetch(
+    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`,
+  );
 
   if (!result.ok) throw new Error('Failed to fetch the data');
 
