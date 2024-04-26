776072cd6f1b01e03dc5c25abbee004fc5b59053
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-04-26 19:13:54 +0500

    Refactoring our code to use the zustand state

--- frontent/web-app/app/_actions/auctionActions.ts
+++ frontent/web-app/app/_actions/auctionActions.ts
@@ -2,13 +2,8 @@
 
 import { Auction, PagedResult } from '@/types';
 
-export const getData = async (
-  pageNumber: number,
-  pageSize: number,
-): Promise<PagedResult<Auction>> => {
-  const result = await fetch(
-    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`,
-  );
+export const getData = async (query: string): Promise<PagedResult<Auction>> => {
+  const result = await fetch(`http://localhost:6001/search${query}`);
 
   if (!result.ok) throw new Error('Failed to fetch the data');
 
