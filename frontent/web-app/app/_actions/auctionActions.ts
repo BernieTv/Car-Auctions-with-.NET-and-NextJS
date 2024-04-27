3dec61abbcb1e9b7b42a900caaae37f66575bd98
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-04-27 18:27:03 +0500

    Testing API authentication

--- frontent/web-app/app/_actions/auctionActions.ts
+++ frontent/web-app/app/_actions/auctionActions.ts
@@ -9,3 +9,19 @@ export const getData = async (query: string): Promise<PagedResult<Auction>> => {
 
   return result.json();
 };
+
+export const updateAuctionTest = async () => {
+  const data = {
+    mileage: Math.floor(Math.random() * 100_000) + 1,
+  };
+
+  const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
+    method: 'PUT',
+    headers: {},
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) return { status: res.status, message: res.statusText };
+
+  return res.statusText;
+};
--- frontent/web-app/app/session/page.tsx
+++ frontent/web-app/app/session/page.tsx
@@ -1,5 +1,6 @@
 import { getSession } from '../_actions/authActions';
 import Heading from '../_components/Heading';
+import AuthTest from './AuthTest';
 
 const Session = async () => {
   const session = await getSession();
@@ -11,6 +12,10 @@ const Session = async () => {
       <div className="bg-blue-200 border-2 border-blue-500">
         <h3 className="text-lg">Session data</h3>
         <pre>{JSON.stringify(session, null, 2)}</pre>
+      </div>
+
+      <div className="mt-4">
+        <AuthTest />
       </div>
     </div>
   );
