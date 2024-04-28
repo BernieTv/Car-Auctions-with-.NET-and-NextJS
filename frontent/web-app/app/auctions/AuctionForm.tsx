3fec406a07cfbb30cd120771a0e6c99ca3fabef4
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-04-28 14:32:19 +0500

    Creating a reusable text input

--- frontent/web-app/app/auctions/AuctionForm.tsx
+++ frontent/web-app/app/auctions/AuctionForm.tsx
@@ -1,5 +1,49 @@
+'use client';
+
+import { FieldValues, useForm } from 'react-hook-form';
+import { Button } from 'flowbite-react';
+
+import Input from '../_components/Input';
+
 const AuctionForm = () => {
-  return <div>AuctionForm</div>;
+  const {
+    control,
+    handleSubmit,
+    setFocus,
+    formState: { isSubmitting, isValid, isDirty, errors },
+  } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="flex flex-col mt-3" onSubmit={handleSubmit(onSubmit)}>
+      <Input label="Make" name="make" control={control} rules={{ required: 'Make is required' }} />
+
+      <Input
+        label="Model"
+        name="model"
+        control={control}
+        rules={{ required: 'Model is required' }}
+      />
+
+      <div className="flex justify-between">
+        <Button outline color="gray">
+          Cancel
+        </Button>
+
+        <Button
+          outline
+          color="success"
+          isProcessing={isSubmitting}
+          disabled={!isValid}
+          type="submit">
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default AuctionForm;
