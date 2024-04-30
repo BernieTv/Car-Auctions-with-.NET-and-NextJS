76d7c0fc102dae45d48bf51683458482d5d11200
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-04-30 12:48:39 +0500

    Adding a producer for the BidPlaced

--- src/SearchService/Consumers/BidPlacedConsumer.cs
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -13,7 +13,6 @@ public class BidPlacedConsumer : IConsumer<BidPlaced>
 
         var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
 
-
         if (context.Message.BidStatus.Contains("Accepted")
             && context.Message.Amount > auction.CurrentHighBid)
         {
